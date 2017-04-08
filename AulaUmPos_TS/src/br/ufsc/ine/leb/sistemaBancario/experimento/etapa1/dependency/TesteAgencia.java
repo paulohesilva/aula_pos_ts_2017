4b64bb4fa5b1592cc65d38dbb10157e9c0b129a5
Paulo Henrique <phs.paulohenriquesilva@gmail.com>
2017-04-08 11:01:33 -0300

    Etapa1 - Exercico4

--- AulaUmPos_TS/src/br/ufsc/ine/leb/sistemaBancario/experimento/etapa1/dependency/TesteAgencia.java
+++ AulaUmPos_TS/src/br/ufsc/ine/leb/sistemaBancario/experimento/etapa1/dependency/TesteAgencia.java
@@ -9,6 +9,8 @@ import br.ufsc.ine.leb.projetos.estoria.Fixture;
 import br.ufsc.ine.leb.projetos.estoria.FixtureSetup;
 import br.ufsc.ine.leb.sistemaBancario.Agencia;
 import br.ufsc.ine.leb.sistemaBancario.Banco;
+import br.ufsc.ine.leb.sistemaBancario.Moeda;
+import br.ufsc.ine.leb.sistemaBancario.SistemaBancario;
 
 @FixtureSetup(TesteBanco.class)
 public class TesteAgencia {
@@ -19,6 +21,8 @@ public class TesteAgencia {
 
 	@Before
 	public void configurar() throws Exception {
+		SistemaBancario sistemaBancario = new SistemaBancario();
+		caixaEconomica = sistemaBancario.criarBanco("CaixaEconomica", Moeda.BRL);
 		caixaEconomicaTrindade = caixaEconomica.criarAgencia("Trindade");
 	}
 
